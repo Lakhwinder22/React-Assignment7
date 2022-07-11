6d5b5875917e832a7dde027e4fb5b46e83a67d35
Lakhwinder Singh <singhlakhwindersingh22@gmail.com>
2022-07-11 12:43:09 +0530

    first commit

---
+++ src/component/Detail.js
@@ -0,0 +1,80 @@
+import React, { createContext, useState } from 'react'
+
+export const add =createContext()
+
+const Detail = (props) => {
+
+    const [students,setStudents]=useState([
+
+{
+
+"Name":"vishnu",
+"Age":"25",
+"Course":"HTML",
+"Batch":"August",
+"id":"1"
+
+},
+
+{
+
+    "Name":"durga",
+    "Age":"27",
+    "Course":"Css",
+    "Batch":"September",
+    "id":"2"
+    
+    },
+    {
+
+        "Name":"divya",
+        "Age":"27",
+        "Course":"React",
+        "Batch":"December",
+        "id":"3"
+        
+        },
+        
+        {
+        
+            "Name":"swathi",
+            "Age":"23",
+            "Course":"Nodejs",
+            "Batch":"January",
+            "id":"4"
+            
+            },
+            {
+        
+                "Name":"raji",
+                "Age":"24",
+                "Course":"Database",
+                "Batch":"April",
+                "id":"5"
+                
+                },
+
+
+    ])
+  return (
+    <div>
+
+
+
+<add.Provider  value={[students,setStudents]}>
+
+
+    {props.children}
+
+
+</add.Provider>
+
+
+
+
+        
+    </div>
+  )
+}
+
+export default Detail;
